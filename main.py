1c447110e0931e1580ea14fec4e66b28e71091b2
Samuel Jakubčík <91385358+SamueloS97@users.noreply.github.com>
2021-10-03 16:14:51 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+def pocet_suborov():
+    while True:
+        cislo = input("Zadaj pocet suborov")
+        try:
+            return int(cislo)
+        except ValueError:
+            print("subor sa nenasiel")
+
+input = []
+cislo_slova = 0
+with open("basnicka.txt") as subor:
+    for slovo in subor:
+        input += slovo.split()
+
+for i in range(pocet_suborov):
+    cislo_slova += 1
+    if cislo_slova == len(input):
+        cislo_slova -= len(input)
+    with open(f"""slovo{cislo_slova}""", mode="w") as subor:
+        print(input[cislo_slova - 1], file=subor)
