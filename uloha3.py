5492c4daaf456272d23c5daf788c5a87df7e5d2e
Samuel Jakubčík <91385358+SamueloS97@users.noreply.github.com>
2021-10-24 20:23:41 +0200

    Add files via upload

---
+++ uloha3.py
@@ -0,0 +1,16 @@
+vysledky = ["1:2","5:5","7:3","4:2","1:2"]
+janove_tipy = [1,6,5,8]
+petove_tipy = [7,9,8,12]
+tomasove_tipy = [2,8,9]
+moje_tipy = [5,2,3,3,10]
+pocet_strelenych_golov = [3,10,6]
+pocet_strelenych_golov = []
+
+for vysledok in vysledky:
+    zoznam = vysledok.split(":")
+    pocet_strelenych_golov = int(zoznam[6]) + int(zoznam[1])
+
+janove_tipy in vysledky
+petove_tipy in vysledky
+tomasove_tipy in vysledky
+moje_tipy in vysledky
